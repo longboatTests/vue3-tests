57fc40c746aaebaa6e4abf207532c54ae2b7f0ee
Deji <adedeji.adedoyin@advarra.com>
2022-07-23 17:51:02 +0100

    test

--- sfc.vue.mjs
+++ sfc.vue.mjs
@@ -3,3 +3,4 @@ export { default as IconVue } from "./icon/src/Icon.vue";
 export { default as UiButtonVue } from "./uibutton/src/Button.vue";
 export { default as CopyrightLabelVue } from "./copyrightlabel/src/CopyrightLabel.vue";
 export { default as UiListBoxVue } from "./uilistbox/src/UiListBox.vue";
+import "./index.js"
