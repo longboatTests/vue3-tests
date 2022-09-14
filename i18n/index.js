3d2cbdedd902acb4d41ac425576b4c3083c39061
Deji <66616843+undefined@users.noreply.github.com>
2022-09-14 15:24:03 +0000

    strst

--- i18n/index.js
+++ i18n/index.js
@@ -1,2 +1,2 @@
-export { default as utils } from '~/i18n/src/utils.js';
-export { default as localelist } from '~/i18n/src/localelist.js';
+export * as utils from '~/i18n/src/utils.js';
+export * as localelist from '~/i18n/src/localelist.js';
